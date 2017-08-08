ac79324b86bab13731a7c69d27d255287e501038
root <mantenimiento.univalle@gmail.com>
2017-08-08 15:56:12 -0500

    Correción creación usuario

--- solicitudes/php/registro_usuarios.php
+++ solicitudes/php/registro_usuarios.php
@@ -1,4 +1,5 @@
-<?
+<?php
+
 session_start();
 $nombre = strtolower(trim($_POST['nombre1']));
 $nombre = ucwords($nombre);
@@ -17,20 +18,17 @@ $dbconn = pg_connect("host=localhost port=5432 dbname=solicitudes_mantenimiento 
 $query = "INSERT INTO usuarios_autorizados_sistema (nombre_usuario, perfil, correo, telefono, extension, login, password, fecha) VALUES ('$nombre', '$perfil', '$correo', '$telefono', '$extension', '$usuario', '$c', 'now()')";
 
 
-if( ! $result = pg_query($dbconn, $query) )
-{
+if( ! $result = pg_query($dbconn, $query) ){
   echo '<script type="text/javascript">confirm("El usuario ya se encuentra registrado");</script>';
 	echo '<script type="text/javascript">' ."\n";
   echo 'window.location="../crear_usuarios.html";';
   echo '</script>';
 	pg_close($dbconn);
-}
-else
-{
+}else{
     pg_close($dbconn);
     echo "<script type='text/javascript'>confirm('El usuario fue registrado correctamente');</script>";
     echo '<script type="text/javascript">' . "\n";
-    echo 'window.location="http://192.168.46.53/mantenimiento/web/index.php";'; 
+    echo 'window.location="http://192.168.46.53/mantenimiento/web/index.php";';
     echo '</script>';
 }
 session_destroy();
